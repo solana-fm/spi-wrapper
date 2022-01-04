a2ae8b4261e8c95c6f76ab5233f5a8a1a7d23e84
nixxholas <nixholas@icloud.com>
2022-01-04 22:35:48 +0800

    src: programs: native_loader: Initial port
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- src/programs/native_loader.rs
+++ src/programs/native_loader.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use avro_rs::Schema;
+use serde::Serialize;
 use crate::{Instruction, InstructionSet};
 
 pub const PROGRAM_ADDRESS: &str = "NativeLoader1111111111111111111111111111111";
@@ -6,10 +9,10 @@ pub const PROGRAM_ADDRESS: &str = "NativeLoader1111111111111111111111111111111";
 /// instruction_properties.
 ///
 /// The function should return a list of instruction properties extracted from an instruction.
-pub async fn fragment_instruction(
+pub async fn fragment_instruction<T: Serialize>(
     // The instruction
     _instruction: Instruction,
-) -> Option<InstructionSet> {
+) -> Option<HashMap<(String, Schema), Vec<T>>> {
     // We don't have anything to work with
     None
 }
