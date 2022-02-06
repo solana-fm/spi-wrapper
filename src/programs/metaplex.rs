8d560c17e8b2b1c7da039c2dbf37fa71c91c5540
nixxholas <nixholas@icloud.com>
2022-02-06 21:32:29 +0800

    src: programs: metaplex: general struct scaffolding, some sample full implementations
    Signed-off-by: nixxholas <nixholas@icloud.com>

--- src/programs/metaplex.rs
+++ src/programs/metaplex.rs
@@ -1,5 +1,6 @@
 use avro_rs::schema::Schema;
 use borsh::BorshDeserialize;
+use itertools::rev;
 use mpl_metaplex::deprecated_state::WinningConfigItem;
 use mpl_metaplex::instruction::MetaplexInstruction;
 use mpl_metaplex::state::{NonWinningConstraint, WinningConfigType, WinningConstraint};
@@ -12,6 +13,7 @@ use crate::{Instruction, TableData, TypedDatum};
 pub const PROGRAM_ADDRESS: &str = "p1exdMJcjVao65QdewkaZRUnU6VPSXhus9n2GzWfh98";
 
 pub const NATIVE_BPF_LOADER_WRITE_TABLE_NAME: &str = "native_bpf_writes";
+pub const METAPLEX_SET_AUCTION_CACHE_TABLE_NAME: &str = "metaplex_set_auction_caches";
 
 lazy_static! {
     pub static ref NATIVE_BPF_LOADER_WRITE_SCHEMA: Schema = Schema::parse_str(
@@ -22,6 +24,24 @@ lazy_static! {
         "fields": [
             {"name": "transaction_hash", "type": "string"},
             {"name": "program", "type": "string"},
+            {"name": "timestamp", "type": "long", "logicalType": "timestamp-millis"}
+        ]
+    }
+    "#
+    )
+    .unwrap();
+    pub static ref METAPLEX_SET_AUCTION_CACHE_TABLE_SCHEMA: Schema = Schema::parse_str(
+        r#"
+    {
+        "type": "record",
+        "name": "metaplex_set_auction_cache",
+        "fields": [
+            {"name": "auction_cache", "type": "string"},
+            {"name": "payer", "type": "string"},
+            {"name": "auction", "type": "string"},
+            {"name": "safety_deposit_box_account", "type": "string"},
+            {"name": "auction_manager", "type": "string"},
+            {"name": "store_key", "type": "string"},
             {"name": "timestamp", "type": "long", "logicalType": "timestamp-millis"}
         ]
     }
@@ -194,27 +214,87 @@ pub struct ValidatedSafetyDepositBoxV2 {
 
 #[derive(Serialize)]
 pub struct RedeemedParticipationBidV3 {
-
+    pub win_index: Option<i64>,
+    pub auction_manager: String,
+    /// Safety deposit token storage account
+    pub safety_deposit_storage_account: String,
+    /// Account containing 1 token of your new mint type.
+    /// MUST be an associated token account of pda [wallet, token program, mint] relative to ata program.
+    pub new_mint_type_account: String,
+    pub bid_redemption_key: String,
+    pub safety_deposit_box_account: String,
+    pub vault_account: String,
+    /// Safety deposit config pda of ['metaplex', program id, auction manager, safety deposit]
+    /// This account will only get used in the event this is an AuctionManagerV2
+    pub safety_deposit_config_account: String,
+    pub auction: String,
+    pub bidder_metadata: String,
+    pub bidder: String,
+    pub payer: String,
+    pub store: String,
+    /// Transfer authority to move the payment in the auction's token_mint coin from the bidder account for the participation_fixed_price
+    /// on the auction manager to the auction manager account itself.
+    pub transfer_authority: String,
+    pub accept_payment_account: String,
+    /// The token account you will potentially pay for the open edition bid with if necessary.
+    pub potential_paying_token_account: String,
+    pub prize_tracking_ticket: String,
+    pub new_metadata_key: String,
+    pub new_edition: String,
+    pub master_edition: String,
+    pub new_token_mint: String,
+    pub edition_pda: String,
+    pub new_mint_mint_authority: String,
+    pub vault_token_metadata: String,
+    pub auction_data_extended_account: String,
+    pub timestamp: i64
 }
 
 #[derive(Serialize)]
 pub struct EndedAuction {
-
+    pub auction_manager: String,
+    pub auction: String,
+    pub auction_extended_data_account: String,
+    pub auction_manager_authority: String,
+    pub store_key: String,
+    pub auction_program: String,
+    pub reveal: Option<Vec<i64>>,
+    pub timestamp: i64
 }
 
 #[derive(Serialize)]
 pub struct SetStoreIndex {
-
+    pub store_index: String,
+    pub payer: String,
+    pub auction_cache: String,
+    pub store_key: String,
+    pub page: i64,
+    pub offset: i64,
+    pub auction_cache_above_current: Option<String>,
+    pub auction_cache_below_current: Option<String>,
+    pub timestamp: i64
 }
 
 #[derive(Serialize)]
 pub struct SetAuctionCache {
-
+    pub auction_cache: String,
+    pub payer: String,
+    pub auction: String,
+    pub safety_deposit_box_account: String,
+    pub auction_manager: String,
+    pub store_key: String,
+    pub timestamp: i64
 }
 
 #[derive(Serialize)]
 pub struct SetStoreV2 {
-
+    pub store_key: String,
+    pub store_config_key: String,
+    pub admin: String,
+    pub payer: String,
+    pub public: bool,
+    pub settings_uri: Option<String>,
+    pub timestamp: i64
 }
 
 /// Extracts the contents of an instruction into small bits and pieces, or what we would call,
@@ -342,8 +422,27 @@ pub async fn fragment_instruction(
                     process_set_store(program_id, accounts, args.public)
                 }
                 MetaplexInstruction::SetStoreV2(args) => {
-                    msg!("Instruction: Set Store V2");
-                    process_set_store_v2(program_id, accounts, args.public, args.settings_uri)
+                    // msg!("Instruction: Set Store V2");
+                    // process_set_store_v2(program_id, accounts, args.public, args.settings_uri)
+
+                    let table_data = TableData {
+                        schema: (*NATIVE_BPF_LOADER_WRITE_SCHEMA).clone(),
+                        table_name: NATIVE_BPF_LOADER_WRITE_TABLE_NAME.to_string(),
+                        data: vec![TypedDatum::Metaplex(MetaplexMainDatum::SetStoreV2(
+                            SetStoreV2 {
+                                store_key: instruction.accounts[0].account.to_string(),
+                                store_config_key: instruction.accounts[1].account.to_string(),
+                                admin: instruction.accounts[2].account.to_string(),
+                                payer: instruction.accounts[3].account.to_string(),
+                                public: args.public,
+                                settings_uri: None,
+                                timestamp: instruction.timestamp,
+                            }))]
+                    };
+
+                    response.push(table_data);
+
+                    Some(response)
                 }
                 MetaplexInstruction::SetWhitelistedCreator(args) => {
                     msg!("Instruction: Set Whitelisted Creator");
@@ -397,20 +496,133 @@ pub async fn fragment_instruction(
                     process_validate_safety_deposit_box_v2(program_id, accounts, safety_deposit_config)
                 }
                 MetaplexInstruction::RedeemParticipationBidV3(args) => {
-                    msg!("Instruction: Redeem Participation Bid V3");
-                    process_redeem_participation_bid(program_id, accounts, false, args.win_index)
+                    // msg!("Instruction: Redeem Participation Bid V3");
+                    // process_redeem_participation_bid(program_id, accounts, false, args.win_index)
+
+                    let table_data = TableData {
+                        schema: (*NATIVE_BPF_LOADER_WRITE_SCHEMA).clone(),
+                        table_name: NATIVE_BPF_LOADER_WRITE_TABLE_NAME.to_string(),
+                        data: vec![TypedDatum::Metaplex(MetaplexMainDatum::RedeemParticipationBidV3(
+                            RedeemedParticipationBidV3 {
+                                win_index: if let Some(win_index) = args.win_index {
+                                    Some(win_index as i64)
+                                } else {
+                                    None
+                                },
+                                auction_manager: instruction.accounts[0].account.to_string(),
+                                safety_deposit_storage_account: instruction.accounts[1].account.to_string(),
+                                new_mint_type_account: instruction.accounts[2].account.to_string(),
+                                bid_redemption_key: instruction.accounts[3].account.to_string(),
+                                safety_deposit_box_account: instruction.accounts[4].account.to_string(),
+                                vault_account: instruction.accounts[5].account.to_string(),
+                                safety_deposit_config_account: instruction.accounts[6].account.to_string(),
+                                auction: instruction.accounts[7].account.to_string(),
+                                bidder_metadata: instruction.accounts[8].account.to_string(),
+                                bidder: instruction.accounts[9].account.to_string(),
+                                payer: instruction.accounts[10].account.to_string(),
+                                store: instruction.accounts[14].account.to_string(),
+                                transfer_authority: instruction.accounts[17].account.to_string(),
+                                accept_payment_account: instruction.accounts[18].account.to_string(),
+                                potential_paying_token_account: instruction.accounts[19].account.to_string(),
+                                prize_tracking_ticket: instruction.accounts[20].account.to_string(),
+                                new_metadata_key: instruction.accounts[21].account.to_string(),
+                                new_edition: instruction.accounts[22].account.to_string(),
+                                master_edition: instruction.accounts[23].account.to_string(),
+                                new_token_mint: instruction.accounts[24].account.to_string(),
+                                edition_pda: instruction.accounts[25].account.to_string(),
+                                new_mint_mint_authority: instruction.accounts[26].account.to_string(),
+                                vault_token_metadata: instruction.accounts[27].account.to_string(),
+                                auction_data_extended_account: instruction.accounts[28].account.to_string(),
+                                timestamp: instruction.timestamp,
+                            }))]
+                    };
+
+                    response.push(table_data);
+
+                    Some(response)
                 }
                 MetaplexInstruction::EndAuction(args) => {
-                    msg!("Instruction: End auction");
-                    process_end_auction(program_id, accounts, args)
+                    // msg!("Instruction: End auction");
+                    // process_end_auction(program_id, accounts, args)
+
+                    let table_data = TableData {
+                        schema: (*NATIVE_BPF_LOADER_WRITE_SCHEMA).clone(),
+                        table_name: NATIVE_BPF_LOADER_WRITE_TABLE_NAME.to_string(),
+                        data: vec![TypedDatum::Metaplex(MetaplexMainDatum::EndAuction(
+                            EndedAuction {
+                                auction_manager: instruction.accounts[0].account.to_string(),
+                                auction: instruction.accounts[1].account.to_string(),
+                                auction_extended_data_account: instruction.accounts[2].account.to_string(),
+                                auction_manager_authority: instruction.accounts[3].account.to_string(),
+                                store_key: instruction.accounts[4].account.to_string(),
+                                auction_program: instruction.accounts[5].account.to_string(),
+                                reveal: if let Some(reveal) = args.reveal {
+                                    Some(vec![reveal.0 as i64, reveal.1 as i64])
+                                } else {
+                                    None
+                                },
+                                timestamp: instruction.timestamp,
+                            }))]
+                    };
+
+                    response.push(table_data);
+
+                    Some(response)
                 }
                 MetaplexInstruction::SetStoreIndex(args) => {
-                    msg!("Instruction: Set Store Index");
-                    process_set_store_index(program_id, accounts, args)
+                    // msg!("Instruction: Set Store Index");
+                    // process_set_store_index(program_id, accounts, args)
+
+                    let table_data = TableData {
+                        schema: (*NATIVE_BPF_LOADER_WRITE_SCHEMA).clone(),
+                        table_name: NATIVE_BPF_LOADER_WRITE_TABLE_NAME.to_string(),
+                        data: vec![TypedDatum::Metaplex(MetaplexMainDatum::SetStoreIndex(
+                            SetStoreIndex {
+                                store_index: instruction.accounts[0].account.to_string(),
+                                auction_cache: instruction.accounts[2].account.to_string(),
+                                payer: instruction.accounts[1].account.to_string(),
+                                store_key: instruction.accounts[3].account.to_string(),
+                                page: args.page as i64,
+                                offset: args.offset as i64,
+                                auction_cache_above_current: if instruction.accounts.len() > 6 {
+                                    Some(instruction.accounts[6].account.to_string())
+                                } else {
+                                    None
+                                },
+                                auction_cache_below_current: if instruction.accounts.len() > 7 {
+                                    Some(instruction.accounts[7].account.to_string())
+                                } else {
+                                    None
+                                },
+                                timestamp: instruction.timestamp,
+                            }))]
+                    };
+
+                    response.push(table_data);
+
+                    Some(response)
                 }
                 MetaplexInstruction::SetAuctionCache => {
-                    msg!("Instruction: Set Auction Cache");
-                    process_set_auction_cache(program_id, accounts)
+                    // msg!("Instruction: Set Auction Cache");
+                    // process_set_auction_cache(program_id, accounts)
+                    let table_data = TableData {
+                        schema: (*METAPLEX_SET_AUCTION_CACHE_TABLE_SCHEMA).clone(),
+                        table_name: METAPLEX_SET_AUCTION_CACHE_TABLE_NAME.to_string(),
+                        data: vec![TypedDatum::Metaplex(MetaplexMainDatum::SetAuctionCache(
+                            SetAuctionCache {
+                                auction_cache: instruction.accounts[0].account.to_string(),
+                                payer: instruction.accounts[1].account.to_string(),
+                                auction: instruction.accounts[2].account.to_string(),
+                                safety_deposit_box_account: instruction.accounts[3].account.to_string(),
+                                auction_manager: instruction.accounts[4].account.to_string(),
+                                store_key: instruction.accounts[5].account.to_string(),
+                                timestamp: instruction.timestamp,
+                            }))]
+                    };
+
+                    response.push(table_data);
+
+                    Some(response)
                 }
             };
         }
